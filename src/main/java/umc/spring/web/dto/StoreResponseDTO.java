8964b1d9db111d84494ef52f8f9941b5598bf4f0
eunji <enji4@naver.com>
2024-12-11 00:56:28 +0900

    feat: 리뷰 조회 응답 DTO 구현
    - ReviewPreviewDTO, ReviewPreviewListDTO 구현

--- src/main/java/umc/spring/web/dto/StoreResponseDTO.java
+++ src/main/java/umc/spring/web/dto/StoreResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -44,5 +45,29 @@ public class StoreResponseDTO {
     public static class ChallengeMissionResultDTO {
         Long userMissionId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO {
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO {
+        String ownerNickname;
+        Float score;
+        String reviewContent;
+        LocalDate createdAt;
     }
 }
