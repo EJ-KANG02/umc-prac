5ebc7c0915e5eb67b356ee744cf9442e3a96346c
eunji <enji4@naver.com>
2024-12-13 21:31:28 +0900

    feat: 진행중인 미션 진행 완료로 바꾸기 API 구현

--- src/main/java/umc/spring/web/controller/UserRestController.java
+++ src/main/java/umc/spring/web/controller/UserRestController.java
@@ -20,6 +20,7 @@ import umc.spring.domain.mapping.UserMission;
 import umc.spring.service.UserService.UserCommandService;
 import umc.spring.service.UserService.UserQueryService;
 import umc.spring.validation.annotation.CheckPage;
+import umc.spring.validation.annotation.ExistMission;
 import umc.spring.validation.annotation.ExistUser;
 import umc.spring.validation.validator.CheckPageValidator;
 import umc.spring.web.dto.StoreRequestDTO;
@@ -82,6 +83,26 @@ public class UserRestController {
         Integer validatedPage = checkPageValidator.validateAndTransformPage(page);
         Page<UserMission> ongoingMissionList = userQueryService.getOngoingMissionListByUserId(userId, validatedPage);
         return ApiResponse.onSuccess(UserConverter.toMissionPreViewListDTO(ongoingMissionList));
+    }
+
+    @GetMapping("/{userId}/missions/ongoing/{missionId}/complete")
+    @Operation(summary = "진행중인 미션 진행 완료로 바꾸기 API", description = "특정 유저가 진행 중인 미션을 진행 완료로 바꾸는 API입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER4001", description = "해당 사용자가 존재하지 않습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "유저의 아이디, path variable 입니다."),
+            @Parameter(name = "missionId", description = "미션의 아이디, path variable 입니다.")
+    })
+    public ApiResponse<UserResponseDTO.CompletedMissionResultDTO> completeUserMission(
+            @ExistUser @PathVariable(name = "userId") Long userId,
+            @ExistMission @PathVariable(name = "missionId") Long missionId) {
+        UserMission userMission = userCommandService.completeMission(userId, missionId);
+        return ApiResponse.onSuccess(UserConverter.toCompletedMissionResultDTO(userMission));
     }
 
 }
