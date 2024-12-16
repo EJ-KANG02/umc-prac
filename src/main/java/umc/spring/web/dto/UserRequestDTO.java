3ff3cca6e6da6b752624a5e82bb8c7dd4cefc8e3
eunji <enji4@naver.com>
2024-12-16 22:11:49 +0900

    feat: 바인딩을 위한 Setter 추가

--- src/main/java/umc/spring/web/dto/UserRequestDTO.java
+++ src/main/java/umc/spring/web/dto/UserRequestDTO.java
@@ -1,10 +1,13 @@
 package umc.spring.web.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 import umc.spring.domain.Region;
+import umc.spring.domain.enums.Role;
 import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.Date;
@@ -13,6 +16,7 @@ import java.util.List;
 public class UserRequestDTO {
 
     @Getter
+    @Setter //@ModelAttribute 가 바인딩 하기 위해 필요
     public static class JoinDto{
 
         @NotNull
@@ -21,14 +25,23 @@ public class UserRequestDTO {
         @NotBlank
         String  userName;
 
-        @NotNull
+        @NotBlank
+        @Email
+        String email;
+
+        @NotBlank
+        String password;
+
+        @NotNull(message = "Gender is required")
         Integer gender;
-        //Date    birth;
 
         @Size(min = 5, max = 12)
         String  address;
 
         @ExistCategories
         List<Long> userFavFoodIdList;
+
+        @NotNull
+        Role role;
     }
 }
